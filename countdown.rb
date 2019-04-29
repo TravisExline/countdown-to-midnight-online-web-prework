2f461d453d2bf74552c8de696f4b1b69f3fd0626
Travis Exline <t.exline22@gmail.com>
2019-04-29 16:28:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,25 @@
 #write your code here
 
-def countdown
+def countdown(counter)
+  counter = 10
+  phrase = "SECOND(S)!"
+  while counter <= 10
+    puts "#{counter} #{phrase}"
+    counter -= 1
+    if counter == 0
+      break
+    end
+  end
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(down)
+  down = 5
+  while down <= 5
+    sleep(1); puts "#{down}"
+    down -= 1
+    if down == 0
+      break
+    end
+  end
 end
